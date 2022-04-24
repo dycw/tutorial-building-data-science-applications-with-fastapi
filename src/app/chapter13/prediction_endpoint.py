8cf7b1d557f648c2c0b51585cf950615befa95d5
Derek Wan <d.wan@icloud.com>
2022-04-25 07:18:22 +0800

    up

--- src/app/chapter13/prediction_endpoint.py
+++ src/app/chapter13/prediction_endpoint.py
@@ -1,5 +1,24 @@
+from typing import Optional
+from typing import cast
+
+from joblib import load
 from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
 
 
 class PredictionInput(BaseModel):
     text: str
+
+
+class PredictionOutput(BaseModel):
+    category: str
+
+
+class NewgroupsModel:
+    model: Optional[Pipeline]
+    targets: Optional[list[str]]
+
+    def load_model(self) -> None:
+        self.model, self.targets = cast(
+            tuple[Pipeline, list[str]], load("newsgroups_model.joblib")
+        )
